8af052554a8497f176ab2534bcd28990f448b59d
Alan <moca.alansing@gmail.com>
2020-11-13 22:19:15 -0600

    Still to do assemblies and unitTest.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,41 +1,20 @@
-﻿/*
- * What will we do?
- * 1.- NORMALIZE VARIABLES -> We use one standard or another but not combined. We will use Camel Caes.
- * 2.- WRITE THE NAME OF THE VARIABLES WELL -> We are not afraid of having long names.
- * 3.- MAKE CONSTANTS -> There are two values ​​that are assigned and are not modified, which are horizontal and vertical.
- * 4.- INDICATE MODIFIERS CORRECTLY -> A cleaner code has everything well specified. (Private, public).
- * 5.- DELETE COMMENTS THAT DO NOT CONTRIBUTE ANYTHING -> Like those of unity.
- * 6.- CLEAN INTERPRETATIONS -> Here we have to read and think about what the code is doing and that is not what we want, for example the if of the update. We will clean that up.
- * 7.- TYPES SIMPLIFICATION WITH VAR -> Within the methods it is not necessary to specify the type of a variable since knowing the type only contributes noise and is already implicit in the name of the variable.
- * = We change the names of the variables again to make it explicit. =
- * 8.- AVOID COMMENTS. Comments don't fix bad code -> Extract horizontal movement and make it more readable.
- * 9.- OPTIMIZATION WITHIN THE EXTRACTION -> It is modified how the vectors are being multiplied so that they multiply scalars together and instead of creating new vectors use the predefined vectors.
- * = More extraction for more readability =
- * 10.- MODIFICATION OF CONDITIONS -> It is the same as cleaning interpretations. We will do it within the extractions.
- * 11.- SAVE CLAUSE -> It is an optimization that is applied to the if's statements and we will apply it to the ones we have just extracted.
- * = More Optimization and standardization of variables = -> FindObjectType.
-*/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [Range(1.0f, 20.0f)]
-    public float movementSpeed;
-
-    [Range(1.0f, 6.0f)]
-    public float distance;
-
-    [Range(1.0f, 90.0f)]
-    public float angle;
+    private const float HorizontalScreenPercentage = 0.1f;
+    private const float VerticalScreenPercentage = 0.1f;
 
     private GameObject player;
 
-    private const float HorizontalScreenPercentage = 0.1f;
-    private const float VerticalScreenPercentage = 0.1f;
+    [Range( 1.0f, 20.0f )]
+    public float movementSpeed;
+
+    [Range( 1.0f, 6.0f )]
+    public float distance;
+
+    [Range( 1.0f, 90.0f )]
+    public float angle;
 
     private void Start()
     {
@@ -108,13 +87,13 @@ public class CameraMovement : MonoBehaviour
 
     public void CenterAtPlayer()
     {
-        float angleRad = Mathf.Deg2Rad * (90 - angle);
+        float angleRad = Mathf.Deg2Rad * ( 90 - angle );
 
-        float yDistanceToTranslate = Mathf.Cos(angleRad) * distance;
-        float xDistanceToTranslate = Mathf.Sin(angleRad) * distance;
+        float yDistanceToTranslate = Mathf.Cos( angleRad ) * distance;
+        float xDistanceToTranslate = Mathf.Sin( angleRad ) * distance;
 
-        var newPosition = player.transform.position + new Vector3(xDistanceToTranslate, yDistanceToTranslate, 0);
+        var newPosition = player.transform.position + new Vector3( xDistanceToTranslate, yDistanceToTranslate, 0 );
         transform.position = newPosition;
-        transform.LookAt(player.transform);
+        transform.LookAt( player.transform );
     }
 }
--- Assets/Scripts/LOL_PlayerMovement.cs
+++ Assets/Scripts/LOL_PlayerMovement.cs
@@ -1,24 +1,4 @@
-﻿/*
- * 12.- OPEN-CLOSED & SINGLE RESPONSABILITY -> We will extract skills management. (Q and W buttons).
- * 12.1.- We create classes for skills extraction.
- * NOTE: The keys that we use for the skill are not removed, the skill only cares about itself. (Thus we fulfill the first principle).
- * NOTE: Real life object name for classes and verb name for methods.
- * 12.2.- Solve the variables they need -> Instead of doing the Monobehaviour transform, we pass it as a parameter to the method. And to instantiate we will do: Object.Instantiate () ... We need to perform so we will do the monobehaviour classes
- * 12.3.- Instances are added to the class in the PlayerMovement class and their methods are called where the keys are pressed. And we apply the guard clause rule to the ifs of the Q key, to remove parentheses and nesting levels.
- * 12.4.- The same applies for the WSpell class.
- * 12.5.- At this point the code is more compact and clean, in addition we have extracted 2 responsibilities from the class.
- * 12.6.- The LOL_PlayerMovement class was moving and applying the skills. The name was not consistent with the responsibilities it had and it still is not because although it does not have the behavior, it does give the orders of when to execute.
- * 13.- Open-Closed -> We will use it to add new abilities and we will not have to modify the player every time we do it.
- * 13.1.- We will disappear the instances of the QSpell and WSpell classes and in their place we will generalize them so that they have a common parent.
- * NOTE: Today this would be ideal but unity still does not allow interfaces to be serialized. And we cannot get them by getcomponent in awake because they are 2 with the same interface, which causes a conflict that does not know which one to use.
- * 13.2.- We will change the interface to an abstract class and its references and instances.
- * NOTE: To pull it in the editor you pull it directly from the component you don't pull the gameObject.
- * 14.- VIEW AGGREGATION -> Now what we will do is that instead of having 2 Spells in the Getcomponents, we will assign a view to have all the ones we want. And the configuration of the key to each spell.
- * 14.1.- We will extract the key.
- * 14.2.- EXTRACTION OF CONFIGURATION CLASS -> We will make a class for the configuration of the spells, we will create an array for the spells and we will extract the handleAtack method in just one.
-*/
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class LOL_PlayerMovement : MonoBehaviour
